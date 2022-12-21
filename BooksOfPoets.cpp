7a47be0676ce507c3e57f2fe7bae641fb5474313
Frozenbaun <120452671+Frozenbaun@users.noreply.github.com>
2022-12-22 02:55:37 +0300

    Create BooksOfPoets.cpp

---
+++ BooksOfPoets.cpp
@@ -0,0 +1,11 @@
+#include "BooksOfPoets.h"
+#include <iostream>
+using namespace std;
+BooksOfPoets::BooksOfPoets()
+{
+	//cout << "Вызов конструктора (Фабрика)" << endl << endl;
+}
+BooksOfPoets::~BooksOfPoets()
+{
+	//cout << "Вызов деструктора (Фабрика)" << endl << endl;
+}
